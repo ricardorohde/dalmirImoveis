121a8bed863d75c427eeb2bfa4f2e223acc40de2
PC\Alberto <alberto.pimentel.94@gmail.com>
2017-04-16 16:02:09 -0300

    alterações

--- 404.php
+++ 404.php
@@ -4,7 +4,7 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
-<title>Castle | 404</title>
+<title>Dalmir Imóveis - Página não encontrada</title>
 <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
 <link rel="stylesheet" type="text/css" href="css/reality-icon.css">
